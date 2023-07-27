07f5cdf0a8465e3cbf6560b9114f08aa80387ba1
Antonio Rodriguez <antoniorodriguezgramajo@gmail.com>
2023-07-27 00:00:11 -0300

    ultimo commit intro

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-export default function Card({ id, name, status, species, gender, origin, image, onClose }) {
-    return (
-      <div>
-          <button onClick={onClose}>X</button>
-         <h2>Name:{name}</h2>
-         <h2>Status:{status}</h2>
-         <h2>Specie:{species}</h2>
-         <h2>Gender:{gender}</h2>
-         <h2>Origin:{origin}</h2>
-         <img src={image} alt='' /> 
-      </div>
+export default function Card({ character, onClose }) {
+   const { id, name, species, gender, origin, image, status } = character;
+   console.log(character, onClose);
+   return (
+     <div id={id}>
+       <button onClick={onClose}>X</button>
+       <h2>{name}</h2>
+       <h2>{status}</h2>
+       <h2>{species}</h2>
+       <h2>{gender}</h2>
+       <h2>{origin.name}</h2>
+       <img src={image} alt="Not found" />
+     </div>
    );
-}
+ }
+ 
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,25 +1,13 @@
-import Card from './Card';
+import Card from "./Card";
 
-export default function Cards({characters}) {
-   return (
-      <div>
-         {
-           characters.map(({id, name, status, species, gender, origin, image})=> {
-           return(
-           <Card
-           key={id}
-           id={id}
-           name={name}
-           status={status}
-           species={species}
-           gender={gender}
-           origin={origin.name}
-           image={image}
-           onClose={() => window.alert('Emulamos que se cierra la card')}
-           />
-           )
-          })
-      }
-      </div>
-      )
+export default function Cards({ characters, onClose }) {
+  return (
+    <div>
+      {characters.map(function (character) {
+        return (
+          <Card key={character.id} character={character} onClose={onClose} />
+        );
+      })}
+    </div>
+  );
 }
