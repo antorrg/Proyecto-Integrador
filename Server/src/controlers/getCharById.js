ca2e746b6b123d6b8fdfa14f944401617c76d3a1
Antonio Rodriguez <antoniorodriguezgramajo@gmail.com>
2023-09-01 00:45:26 -0300

    Commit Promises 03

---
+++ Server/src/controlers/getCharById.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+
+const getCharById = (res, id) => {
+ axios.get(`https://rickandmortyapi.com/api/character/:${id}`)
+    .then(({data}) =>{
+        const {name, status, species, origin = origin.name, gender, image} = data;
+        const character ={name, gender, origin, species, image, id, status}
+        res.writeHead(200, {'content-type': 'application/json'})
+        res.end(JSON.stringify(character))
+     })
+    .catch((reason) =>{
+        res.writeHead(500, {'content-type': 'text/plain'})
+        res.end(reason.message)
+    })
+}
+
+module.exports = getCharById;
